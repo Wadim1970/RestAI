4378c78638dac91e005a9127b85f225cd4d0582a
Wadim1970 <fomenko1970@yandex.ru>
2026-03-11 16:25:07 +0300

    Wrap App component with ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css' // Закомментировали или удалили
-import './styles.css' // <-- Теперь импортируем наш файл
-
+import './styles.css'
 import App from './App.jsx'
+import { ThemeProvider } from './components/ThemeProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
